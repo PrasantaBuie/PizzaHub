35785834c11ea4d8fbfa612c149a33271d876743
unknown <prasantabanerjee.buie@gmail.com>
2021-06-15 18:36:18 +0530

    adding all react files

---
+++ models/userModel.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+const userSchema=mongoose.Schema({
+    name:{type:String,require},
+    email:{type:String,require},
+    password:{type:String,require},
+    isAdmin:{type:Boolean,default:false}
+},{
+    timestamps:true,
+})
+module.exports=mongoose.model('users',userSchema) //collection name,schema
